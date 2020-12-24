2a2c1b8e38b5beb45fd47dc270f00f93967a2435
98sunyoung <98sunyoung@gmail.com>
2020-12-24 09:37:44 +0900

    PDF 기능

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:counter_bloc/pdf.dart';
 import 'package:flutter/material.dart';
 
 import 'counter/counter.dart';
@@ -5,7 +6,37 @@ import 'counter/counter.dart';
 /// {@template counter_app}
 /// A [MaterialApp] which sets the `home` to [CounterPage].
 /// {@endtemplate}
-class CounterApp extends MaterialApp {
-  /// {@macro counter_app}
-  const CounterApp({Key key}) : super(key: key, home: const CounterPage());
+// class CounterApp extends MaterialApp {
+/// {@macro counter_app}
+// const CounterApp({Key key}) : super(key: key, home: const CounterPage());
+// }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return new MaterialApp(
+      home: new CounterApp(),
+    );
+  }
+}
+
+class CounterApp extends StatelessWidget {
+  final PageController pageController = PageController(
+    initialPage: 0,
+  );
+
+//  출처: https://spiralmoon.tistory.com/entry/Flutter-여러-페이지를-한-화면에서-PageView [Spiral Moon's programming blog]
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: pageController,
+        children: [
+          CounterPage(),
+          PDFPage()
+        ],
+      ),
+    );
+  }
 }
