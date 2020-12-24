42506aee9063ba8ab7559f007dc8129cfbd192db
Yeonwoo Lim <1213terry@naver.com>
2020-12-24 13:31:47 +0900

    [ADD] 네트워크 연결 여부 체크

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:counter_bloc/pdf.dart';
 import 'package:flutter/material.dart';
+import 'package:connectivity/connectivity.dart';
+import 'package:flutter/services.dart';
 
 import 'counter/counter.dart';
 
@@ -14,29 +18,95 @@ import 'counter/counter.dart';
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    return new MaterialApp(
-      home: new CounterApp(),
+    return MaterialApp(
+      home: CounterApp(),
     );
   }
 }
 
 class CounterApp extends StatelessWidget {
+//  출처: https://spiralmoon.tistory.com/entry/Flutter-여러-페이지를-한-화면에서-PageView [Spiral Moon's programming blog]
+  @override
+  Widget build(BuildContext context) {
+    return HomePage();
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final PageController pageController = PageController(
     initialPage: 0,
   );
+  String _connectionStatus = 'Unknown';
+  final Connectivity _connectivity = Connectivity();
+  StreamSubscription<ConnectivityResult> _connectivitySubscription;
 
-//  출처: https://spiralmoon.tistory.com/entry/Flutter-여러-페이지를-한-화면에서-PageView [Spiral Moon's programming blog]
+  @override
+  void initState() {
+    super.initState();
+    initConnectivity();
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _connectivitySubscription.cancel();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
         controller: pageController,
-        children: [
-          CounterPage(),
-          PDFPage()
-        ],
+        children: _connectionStatus == 'ConnectivityResult.none'
+            ? [
+                Scaffold(
+                  appBar: AppBar(title: Text('네트워크를 연결해 주세요.')),
+                  body: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                ),
+              ]
+            : [
+                CounterPage(),
+                PDFPage(),
+              ],
       ),
     );
+  }
+
+  Future<void> initConnectivity() async {
+    ConnectivityResult result;
+    try {
+      result = await _connectivity.checkConnectivity();
+    } on PlatformException catch (e) {
+      print(e.toString());
+    }
+
+    if (!mounted) {
+      return Future.value(null);
+    }
+
+    return _updateConnectionStatus(result);
+  }
+
+  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+    print(result);
+    switch (result) {
+      case ConnectivityResult.wifi:
+      case ConnectivityResult.mobile:
+      case ConnectivityResult.none:
+        setState(() => _connectionStatus = result.toString());
+        break;
+      default:
+        setState(() => _connectionStatus = 'Failed to get connectivity.');
+        break;
+    }
   }
 }
